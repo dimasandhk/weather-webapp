c7e221ca7a3af375465005cff09bfa2f3d241047
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-05-06 16:10:21 +0700

    View Engine + Static Directory

--- src/app.js
+++ src/app.js
@@ -1,28 +1,39 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
+
+// Variables and path for express config
 const port = process.env.PORT || 3000;
 const publicDirPath = path.join(__dirname, "../public");
+const componentsPath = path.join(__dirname, "../views/components");
 
+// Setup handlebars engine + static dir
+app.set("view engine", "hbs");
 app.use(express.static(publicDirPath));
+hbs.registerPartials(componentsPath);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About Page</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send("<h1>About Page</h1>");
-});
-
-app.get("/weather", (req, res) => {
-  res.send({
-    location: "Jakarta",
+app.get("/", (_req, res) => {
+  res.render("index", {
+    title: "Hello World",
   });
 });
 
-app.listen(port, () => console.log(`Server is up on port ${port}`));
+app.get("/about", (_req, res) => {
+  res.render("about");
+});
+
+app.get("/weather", (_req, res) => {
+  res.send({
+    location: "Jakarta",
+    forecast: {
+      temp: 30,
+      desc: "Cloudy",
+    },
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Server is up on http://localhost:${port}/`)
+);
